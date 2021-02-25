d0f1c9b83cfc52a72e907129895f965569b595c1
TriyasRPL1 <triyaswahyusaputra99@example.com>
2021-02-24 17:28:53 -0800

    Implementasi Struktur Kelas Aplikasi Kasir

---
+++ src/classes/DaftarMenu.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+*/
+import classes.Menu;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class DaftarMenu {
+    private ArrayList<Menu> daftarMenu;
+    
+    public DaftarMenu() { }
+    
+    public void tambahMenu (Menu menu) { }
+    
+    public void getMenuByKategori (String kategori) { }
+    
+    public void tampilDaftarMenu () { }
+}
